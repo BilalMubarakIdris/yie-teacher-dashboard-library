767588f3a7804755d8e51dd4f189a87642cc17fa
CodeWithBilal <bilalmubarakidris@gmail.com>
2022-02-21 07:16:55 +0530

    adding teachers, student and admin calender component with some functionality

--- src/App.js
+++ src/App.js
@@ -4,6 +4,12 @@ import Navbar from './components/Navbar';
 import ViewUploadedContent from './components/ViewUploadedContent'
 import CreateNewContent from './components/CreateNewContent'
 import ViewPreviousContent from './components/ViewPreviousContent'
+import Calendar from './components/Calendar'
+import CreateEvent from './components/CreateEvent'
+import GenerateSchedule from "./components/studentCalendar/GenerateSchedule"
+import StudentCalendar from './components/studentCalendar/StudentCalendar'
+import AdminEvent from './components/adminCalendar/AdminEvent'
+import AdminCalendar from './components/adminCalendar/AdminCalendar'
 
 function App() {
   return (
@@ -14,6 +20,15 @@ function App() {
         <Route path="/view_uploaded_content" element={<ViewUploadedContent />}/>
         <Route path="/view_previous_content" element={<ViewPreviousContent />}/>
         <Route path="/create_new_content" element={<CreateNewContent />}/>
+        <Route path="/calendar" element={<Calendar />}>
+          <Route path="create-event" element={<CreateEvent />}/>
+        </Route>
+        <Route path="/calendar1" element={<StudentCalendar />}>
+          <Route path="student-calendar" element={<GenerateSchedule />}/>
+        </Route>
+        <Route path="/calendar2" element={<AdminCalendar />}>
+          <Route path="admin-calendar" element={<AdminEvent/>}/>
+        </Route>
       </Routes>
     </div>
   );
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,8 +7,17 @@ function Navbar() {
     <>
       <header className="border-solid border-2 border-green-500 h-screen">
         <ul className="list-none p-0">
-          <li className="bg-blue-500  py-2 px-0 my-0.5">
+          <li className="bg-blue-500 py-2 px-0 my-0.5">
             <Link className="py-0 px-20 text-white" to="/">Library</Link>
+          </li>
+          <li className="bg-blue-500 py-2 px-0 my-0.5">
+            <Link className="py-0 px-20 text-white" to="/calendar">Calendar</Link>
+          </li>
+          <li className="bg-blue-500 py-2 px-0 my-0.5">
+            <Link className="py-0 px-20 text-white" to="/calendar1">Calendar1</Link>
+          </li>
+          <li className="bg-blue-500 py-2 px-0 my-0.5">
+            <Link className="py-0 px-20 text-white" to="/calendar2">Calendar2</Link>
           </li>
         </ul>
       </header>
